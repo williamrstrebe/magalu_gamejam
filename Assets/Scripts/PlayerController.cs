722f9d59d4c99e0a1d760624a1338adf9d931772
William Ricardo Strebe <william.ricardo.strebe@gmail.com>
2022-06-25 19:02:09 -0300

    Main Menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,11 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     private float moveX;
     public float moveSpeed;
+    public Animator animator;
+
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -16,14 +19,17 @@ public class PlayerController : MonoBehaviour
     {
 
         moveX = Input.GetAxisRaw("Horizontal");
+        Debug.Log(moveX);
+        animator.SetBool("walk", moveX != 0);
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveX * moveSpeed, 0);
-        if (Mathf.Abs(rb.velocity.x) > Mathf.Epsilon)
+        bool moveHorizontaly = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (moveHorizontaly)
         {
-            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1);
         }
 
     }
